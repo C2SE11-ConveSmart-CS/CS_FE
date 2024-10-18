38a313c3b849bf5ec93e0fd076ab42a6d6f8c8c2
Phạm Đức Giang <ducgiang01@192.168.2.43>
2024-10-18 09:11:18 +0700

    update appjs

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import SignIn from './pages/SignIn/SignIn'
 import SignUp from './pages/SignUp/SignUp'
 import ProfileAccount from './pages/Profile Account/ProfileAccount'
 import ChangePassword from './pages/Profile Account/ChangePassword'
+import Login from './components/LoginPage/LoginPage'
+import Register from './components/RegisterPage/RegisterPage'
+import ProfilePage from './components/ProfileComponents/ProfilePage'
+
+
 
 const App = () => {
   return (
@@ -14,10 +19,15 @@ const App = () => {
         <Route path="/" element={<Landing />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
         <Route path="*" element={<NotFound />} />
         <Route path="/profileaccount" element={<ProfileAccount />} />
         <Route path="/changepassword" element={<ChangePassword />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<ProfilePage />} />
+
+
       </Routes>
     </BrowserRouter>
   )
